518e877105c55b7c77200b3a9d75e8bb3f6b8b81
unknown <sajitahame56@gmail.com.com>
2017-12-02 16:20:31 +0530

    make it better

--- app.py
+++ app.py
@@ -13,7 +13,42 @@ def home():
 def welcome():
     return render_template('welcome.html')  # render a template
 
+@app.route('/newpage1')
+def newpage1():
+    return render_template('main.html')
+
 
 # # start the server with the 'run()' method
 if __name__ == '__main__':
     app.run(debug=True)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+# from flask import Flask, render_template
+
+# # create the application object
+# app = Flask(__name__)
+
+# # use decorators to link the function to a url
+# @app.route('/')
+# def home():
+    # return "Hello, World! This is the app develop and design for the \n education purpose only anyone who are interested can contact mamu on the given phone number ok!!!!!!!!!"  # return a string
+
+# @app.route('/welcome')
+# def welcome():
+    # return render_template('welcome.html')  # render a template
+
+
+# # # start the server with the 'run()' method
+# if __name__ == '__main__':
+    # app.run(debug=True)
