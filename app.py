92d9e0668acb7b75459d0f3efd004df869588665
unknown <sajitahame56@gmail.com.com>
2017-12-02 17:00:25 +0530

    make it better

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 # create the application object
 app = Flask(__name__)
@@ -16,6 +16,16 @@ def welcome():
 @app.route('/newpage1')
 def newpage1():
     return render_template('main.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 
 # # start the server with the 'run()' method
