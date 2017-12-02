f082c944c5b8a35e8e278fdf3f02279d649cab51
unknown <sajitahame56@gmail.com.com>
2017-12-02 16:05:52 +0530

    make it better

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+# import the Flask class from the flask module
 from flask import Flask, render_template
 
+# create the application object
 app = Flask(__name__)
 
+# use decorators to link the function to a url
 @app.route('/')
-def homepage():
-    return render_template("index.html")
+def home():
+    return "Hello, World! This is the app develop and design for the education purpose only anyone who are interested can contact mamu on the given phone number ok!!!!!!!!!"  # return a string
 
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')  # render a template
 
-if __name__ == "__main__":
-    app.run()
+# # start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
