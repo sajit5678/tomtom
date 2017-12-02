753a3b056f9cceb21a5258d99653224536ebdb81
unknown <sajitahame56@gmail.com.com>
2017-12-02 15:25:26 +0530

    make it better

--- app.py
+++ app.py
@@ -30,18 +30,18 @@ def home():
 def welcome():
     return render_template('welcome.html')  # render a template
 
-# route for handling the login page logic
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in.')
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+# # route for handling the login page logic
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+    # error = None
+    # if request.method == 'POST':
+        # if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            # error = 'Invalid Credentials. Please try again.'
+        # else:
+            # session['logged_in'] = True
+            # flash('You were logged in.')
+            # return redirect(url_for('home'))
+    # return render_template('login.html', error=error)
 
 @app.route('/logout')
 @login_required
