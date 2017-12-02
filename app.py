804829e2c0a660aeecdaef126f465c8fc6713acb
unknown <sajitahame56@gmail.com.com>
2017-12-02 16:10:59 +0530

    make it better

--- app.py
+++ app.py
@@ -7,11 +7,15 @@ app = Flask(__name__)
 # use decorators to link the function to a url
 @app.route('/')
 def home():
-    return "Hello, World! This is the app develop and design for the education purpose only anyone who are interested can contact mamu on the given phone number ok!!!!!!!!!"  # return a string
+    return "Hello, World! This is the app develop and design for the \n education purpose only anyone who are interested can contact mamu on the given phone number ok!!!!!!!!!"  # return a string
 
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html')  # render a template
+
+@app.route('/newpage1')
+def welcome():
+    return render_template('main.html')
 
 # # start the server with the 'run()' method
 if __name__ == '__main__':
