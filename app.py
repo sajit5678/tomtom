c73b7d4cd7b0058e3baa015465d91163d6d2f3a2
najfamilyexpenditure <sajitahamed56@gmail.com>
2018-05-09 14:40:33 +0530

    make it better

--- app.py
+++ app.py
@@ -19,23 +19,23 @@ class deducted(db.Model):
 	bal = db.Column(db.String(100))
 
 	def __init__(self, date, name, city, addr, transfer, bal):
-		   self.date = date
-		   self.name = name
-		   self.city = city
-		   self.addr = addr
-		   self.transfer = transfer
-		   self.bal = bal
+		self.date = date
+		self.name = name
+		self.city = city
+		self.addr = addr
+		self.transfer = transfer
+		self.bal = bal
 
 # login required decorator
 def login_required(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('login'))
-    return wrap
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return f(*args, **kwargs)
+		else:
+			flash('You need to login first.')
+			return redirect(url_for('login'))
+	return wrap
 	
 # use decorators to link the function to a url
 @app.route('/')
@@ -45,67 +45,69 @@ def show_all():
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'najfamily' or request.form['password'] != 'ourfamily':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            session['logged_in'] = True
-            flash('Log in again')
-            return redirect(url_for('show_all'))
-    return render_template('login.html', error=error)
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != 'tomba' or request.form['password'] != 'tombi':
+			error = 'Invalid Credentials. Please try again.'
+		else:
+			session['logged_in'] = True
+			flash('Log in again')
+			return redirect(url_for('show_all'))
+	return render_template('login.html', error=error)
 	
 @app.route('/add', methods = ['GET', 'POST'])
 @login_required
 def add():
 	if request.method == 'POST':
-	  if not request.form['name'] or not request.form['tra']:
-		 flash('Please enter all the fields', 'error')
-	  else:
-		data = deducted.query.all()
-		b = request.form['name']
-		e = request.form['tra']
-		k = request.form['bal']
-		j = request.form['date']
-		if data:
-			for col in data:
-				h = col.bal
-				g = int(h) + int(e)
+		if not request.form['name'] or not request.form['tra']:
+			flash('Please enter all the fields', 'error')
 		else:
-			g = e
-			#g = int(h) + int(e)
-		deduct = deducted(j, b, '', k, e, g)
+			data = deducted.query.all()
+			b = request.form['name']
+			e = request.form['tra']
+			k = request.form['bal']
+			j = request.form['date']
+			if data:
+				for col in data:
+					h = col.bal
+					g = int(h) + int(e)
+			else:
+				g = e
+				#g = int(h) + int(e)
+			deduct = deducted(j, b, '', k, e, g)
 
-		db.session.add(deduct)
-		db.session.commit()
-		flash('Record was successfully added')
-		return redirect(url_for('show_all'))
+			db.session.add(deduct)
+			db.session.commit()
+			db.session.close()
+			#flash('Record was successfully added')
+			return redirect(url_for('show_all'))
 		
 	return render_template('add.html')
 
 @app.route('/use', methods = ['GET', 'POST'])
 @login_required
 def use():
-   if request.method == 'POST':
-      if not request.form['name'] or not request.form['deduct']:
-         flash('Please enter all the fields', 'error')
-      else:
-		data = deducted.query.all()
-		if data:
-			for col in data:
-				h = col.bal
-		b = request.form['name']
-		c = request.form['deduct']
-		d = request.form['item']
-		j = request.form['date']
-		g = int(h)-int(c)
-		deduct = deducted(j, b, c, d, '', g)
+	if request.method == 'POST':
+		if not request.form['name'] or not request.form['deduct']:
+			flash('Please enter all the fields', 'error')
+		else:
+			data = deducted.query.all()
+			if data:
+				for col in data:
+					h = col.bal
+			b = request.form['name']
+			c = request.form['deduct']
+			d = request.form['item']
+			j = request.form['date']
+			g = int(h)-int(c)
+			deduct = deducted(j, b, c, d, '', g)
 
-		db.session.add(deduct)
-		db.session.commit()
-		flash('Record was successfully added')
-		return redirect(url_for('show_all'))
-   return render_template('use.html')
+			db.session.add(deduct)
+			db.session.commit()
+			db.session.close()
+			flash('Record was successfully added')
+			return redirect(url_for('show_all'))
+	return render_template('use.html')
 
 @app.route('/logout')
 @login_required
